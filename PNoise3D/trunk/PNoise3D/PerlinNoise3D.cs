517e5ff66b4947e7a789e4f6b6c22e2c5c5b604d
ppjunker@gmail.com <ppjunker@gmail.com@beb4136b-5abe-93ac-1838-bee8c9d71782>
2012-02-09 14:32:41 +0000

    some catch and try for error suppressing

--- PNoise3D/trunk/PNoise3D/PerlinNoise3D.cs
+++ PNoise3D/trunk/PNoise3D/PerlinNoise3D.cs
@@ -45,13 +45,17 @@ namespace PNoise3D
 
         private void generateNoise()
         {
-            noise = new Single[noiseDimension, noiseHeight, noiseDimension];
-            Random noiseRandom = new Random(seed);
+            try
+            {
+                noise = new Single[noiseDimension,noiseHeight,noiseDimension];
+                Random noiseRandom = new Random(seed);
 
-            for (Int32 x = 0; x < noiseDimension; ++x)
-                for (Int32 y = 0; y < noiseHeight; ++y)
-                    for (Int32 z = 0; z < noiseDimension; ++z)
-                        noise[x, y, z] = ((Single) (noiseRandom.NextDouble()) - 0.5f)*2.0f;
+                for (Int32 x = 0; x < noiseDimension; ++x)
+                    for (Int32 y = 0; y < noiseHeight; ++y)
+                        for (Int32 z = 0; z < noiseDimension; ++z)
+                            noise[x, y, z] = ((Single) (noiseRandom.NextDouble()) - 0.5f)*2.0f;
+            }
+            catch { }
         }
 
         public Single GetValue(Single x, Single y, Single z, Single Frequency, Single Amplitude, Single Persistance, Int32 Octaves)
@@ -83,30 +87,36 @@ namespace PNoise3D
 
         private Single getSmoothNoise(Single x, Single y, Single z)
         {
-            Single FractionX = x - (Int32)x;
-            Single FractionY = y - (Int32)y;
-            Single FractionZ = z - (Int32)z;
+            try
+            {
 
-            Int32 X1 = ((Int32)x + noiseDimension) % noiseDimension;
-            Int32 Z1 = ((Int32)z + noiseDimension) % noiseDimension;
-            Int32 Y1 = ((Int32)y + noiseHeight) % noiseHeight;
+                Single FractionX = x - (Int32) x;
+                Single FractionY = y - (Int32) y;
+                Single FractionZ = z - (Int32) z;
 
-            Int32 X2 = ((Int32)x + noiseDimension - 1) % noiseDimension;
-            Int32 Z2 = ((Int32)z + noiseDimension - 1) % noiseDimension;
-            Int32 Y2 = ((Int32)y + noiseHeight - 1) % noiseHeight;
+                Int32 X1 = ((Int32) x + noiseDimension)%noiseDimension;
+                Int32 Z1 = ((Int32) z + noiseDimension)%noiseDimension;
+                Int32 Y1 = ((Int32) y + noiseHeight)%noiseHeight;
 
-            Single value = 0;
+                Int32 X2 = ((Int32) x + noiseDimension - 1)%noiseDimension;
+                Int32 Z2 = ((Int32) z + noiseDimension - 1)%noiseDimension;
+                Int32 Y2 = ((Int32) y + noiseHeight - 1)%noiseHeight;
 
-            value += FractionX * FractionY * FractionZ * noise[X1, Y1, Z1];
-            value += FractionX * FractionY * (1 - FractionZ) * noise[X1, Y1, Z2];
-            value += FractionX * (1 - FractionY) * FractionZ * noise[X1, Y2, Z1];
-            value += FractionX * (1 - FractionY) * (1 - FractionZ) * noise[X1, Y2, Z2];
-            value += (1 - FractionX) * FractionY * FractionZ * noise[X2, Y1, Z1];
-            value += (1 - FractionX) * FractionY * (1 - FractionZ) * noise[X2, Y1, Z2];
-            value += (1 - FractionX) * (1 - FractionY) * FractionZ * noise[X2, Y2, Z1];
-            value += (1 - FractionX) * (1 - FractionY) * (1 - FractionZ) * noise[X2, Y2, Z2];
+                Single value = 0;
 
-            return value;
+                value += FractionX*FractionY*FractionZ*noise[X1, Y1, Z1];
+                value += FractionX*FractionY*(1 - FractionZ)*noise[X1, Y1, Z2];
+                value += FractionX*(1 - FractionY)*FractionZ*noise[X1, Y2, Z1];
+                value += FractionX*(1 - FractionY)*(1 - FractionZ)*noise[X1, Y2, Z2];
+                value += (1 - FractionX)*FractionY*FractionZ*noise[X2, Y1, Z1];
+                value += (1 - FractionX)*FractionY*(1 - FractionZ)*noise[X2, Y1, Z2];
+                value += (1 - FractionX)*(1 - FractionY)*FractionZ*noise[X2, Y2, Z1];
+                value += (1 - FractionX)*(1 - FractionY)*(1 - FractionZ)*noise[X2, Y2, Z2];
+
+                return value;
+            }
+            catch { }
+            return 0;
         }
 
         #endregion ---------------------------------------------------------------------------------
